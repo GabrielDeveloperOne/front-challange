295f592b26ae7756173460b730a2d561475e7a23
Gabriel <gabriel.developer@outlook.com>
2020-01-12 13:49:11 -0300

    adicionando validação de formulario

--- src/js/app.js
+++ src/js/app.js
@@ -1,9 +1,12 @@
 
+/* Adicicionando $ ao escopo global */
+let $ = document.querySelector.bind(document);
+
 
 
 
 /* Utilização de Regex para mascara do telefone */
-document.getElementById('phone').addEventListener('input', function (e) {
+$('#phone').addEventListener('input', (e) => {
     let tel = e.target.value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,5})(\d{0,4})/);
     e.target.value = !tel[2] ? tel[1] : '(' + tel[1] + ') ' + tel[2] + (tel[3] ? '-' + tel[3] : '');
   });
